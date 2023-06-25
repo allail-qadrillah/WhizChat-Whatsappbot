f3ec35f4079a7e1657f6f52f28d7fd9caa15dc9e
allail-qadrillah <89723505+allail-qadrillah@users.noreply.github.com>
2023-06-25 18:59:10 +0000

    cek chat dari group

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ kirim foto = ngubah foto jadi sticker
 kirim video maks 5 detik = jadiin sticker GIF
 
 #ask_question = menjawab semua pertanyaan  
- ex : #ask buatkan format chat dengan dosen
+ ex : #ask give me new hot news
  
 #speech_text = convert text ke suara
  ex : #speech hai, udah makan belom
@@ -37,7 +37,7 @@ coba liat disini https://bit.ly/cara-pakai-whatsappbot
 console.log('server started ...')
 console.log(getJakartaTime().toString())
 
-mongoose.connect(process.env['MONGGO_URL']).then(async() => {
+mongoose.connect(process.env['MONGGO_URL']).then(async () => {
   const { stdout: chromiumPath } = await exec("which chromium");
 
   const store = new MongoStore({ mongoose: mongoose });
@@ -54,74 +54,84 @@ mongoose.connect(process.env['MONGGO_URL']).then(async() => {
       executablePath: chromiumPath.trim()
     }
   });
-  
+
   client.on('message', async msg => {
     const text = msg.body.toLowerCase() || '';
     const chat = await msg.getChat()
 
-    if (text === 'p') {
-      chat.sendStateTyping()
-      console.log(`${ msg._data.notifyName } | ${ msg.deviceType } cek status`)
-      msg.reply('online 👌');
+    console.log(chat.isGroup)
+    // jika pesan bukan dari group
+    if (!chat.isGroup) {
+      if (text === 'p') {
+        chat.sendStateTyping()
+        console.log(`${msg._data.notifyName} | ${msg.deviceType} cek status`)
+        msg.reply('online 👌');
 
-    } else if (text.includes("#ask")){
-      chat.sendStateTyping()
-      console.log(`${ msg._data.notifyName } | ${ msg.deviceType } AI Request`)
-      await chatAIHandler(text, client, msg, MessageMedia)
+      } else if (text.includes("#ask")) {
+        chat.sendStateTyping()
+        console.log(`${msg._data.notifyName} | ${msg.deviceType} AI Request`)
+        await chatAIHandler(text, client, msg, MessageMedia)
 
-    } else if (msg.hasMedia){
-      try {
-        console.log(`${ msg._data.notifyName } | ${ msg.deviceType } image to sticker`)
-        convertFotoToSticker(client, msg, MessageMedia)
-      } catch (error) {
-        client.reply(msg.from, "gabisa dijadiin sticker 😔")
+      } else if (msg.hasMedia) {
+        try {
+          console.log(`${msg._data.notifyName} | ${msg.deviceType} image to sticker`)
+          convertFotoToSticker(client, msg, MessageMedia)
+        } catch (error) {
+          client.reply(msg.from, "gabisa dijadiin sticker 😔")
+        }
+
+      } else if (text.includes("#imagine")) {
+        console.log('Generate Image')
+        await imageGeneratorAIHandler(text, client, msg, MessageMedia)
+
+      } else if (text.includes("#speech")) {
+        chat.sendStateRecording()
+        console.log(`${msg._data.notifyName} | ${msg.deviceType} text to speech`)
+        await textToSpeechHandler(text, client, msg, MessageMedia)
+
+      } else if (text.includes("#fakespeech")) {
+        console.log(`${msg._data.notifyName} fake text to speech`)
+        await fakeVoiceTextToSpeechHandler(text, client, msg, MessageMedia)
+
+      } else {
+        chat.sendStateTyping()
+        client.sendMessage(msg.from, menu)
       }
-      
-    } else if (text.includes("#imagine")){
-      console.log('Generate Image')
-      await imageGeneratorAIHandler(text, client, msg, MessageMedia)   
-    
-    } else if (text.includes("#speech")){
-      chat.sendStateRecording()
-      console.log(`${ msg._data.notifyName } | ${ msg.deviceType } text to speech`) 
-      await textToSpeechHandler(text, client, msg, MessageMedia)   
-      
-    } else if (text.includes("#fakespeech")){
-      console.log(`${ msg._data.notifyName } fake text to speech`) 
-      await fakeVoiceTextToSpeechHandler(text, client, msg, MessageMedia)   
-      
-    }else {
-      chat.sendStateTyping()
-      client.sendMessage(msg.from, menu)
-      let button = new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer');
-      client.sendMessage(msg.from, button);
+      // jika dari group
+    } else {
+      if (text.includes("#ask")) {
+        chat.sendStateTyping()
+        console.log(`${msg._data.notifyName} | ${msg.deviceType} AI Request`)
+        await chatAIHandler(text, client, msg, MessageMedia)
+      }
     }
 
+
+
+
+    client.on('qr', (qr) => {
+      qrcode.generate(qr, { small: true });
+    });
+
+    client.on('remote_session_saved', () => {
+      console.log('Session saved remotely')
+    })
+
+    client.on('ready', () => {
+      console.log('Client is ready!');
+      const number = "+6282277396265";
+
+      const text = "Server Started!";
+      const chatId = number.substring(1) + "@c.us";
+      client.sendMessage(chatId, text);
+
+    });
+
+    app.get('/', (req, res) => {
+      res.send('Online👌');
+    })
+
+    app.listen(3000, () => console.log('app listening on port 3000'))
+    client.initialize();
   });
-
-  
-  client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-  });
-
-  client.on('remote_session_saved', () => {
-    console.log('Session saved remotely')
-  })
-
-  client.on('ready', () => {
-    console.log('Client is ready!');
-    const number = "+6282277396265";
-
-    const text = "Server Started!";
-    const chatId = number.substring(1) + "@c.us";
-    client.sendMessage(chatId, text);
-
-  });
-
-  app.get('/', (req, res) => {
-    res.send('Online👌');
-  })
-
-  app.listen(3000, () => console.log('app listening on port 3000'))
-  client.initialize();
-});
+})
